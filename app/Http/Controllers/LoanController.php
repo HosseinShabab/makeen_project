f00d991fc7215e0870db897cb5d34c4210c38988
hossein <shabab1831@gmail.com>
2024-07-09 00:06:08 +0330

    creating loan controller

---
+++ app/Http/Controllers/LoanController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoanController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy(){
+
+    }
+}
