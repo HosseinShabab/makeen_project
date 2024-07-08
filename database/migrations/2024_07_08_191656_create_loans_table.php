02d126048a2d6004a69ffa9a81fd466d5f83d2cc
hossein <shabab1831@gmail.com>
2024-07-08 23:56:24 +0330

    creating loans migration

---
+++ database/migrations/2024_07_08_191656_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("request_number");
+            $table->unsignedBigInteger("loan_price");
+            $table->text("description");
+            $table->enum('type',["normal","necessary"]);
+            $table->unsignedBigInteger("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
