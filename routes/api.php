cd27f1a471de4a69ec8c341d402fb84a64cf7f15
Arman <alibabooo1384@gmail.com>
2024-07-08 11:03:15 -0700

    Write the Route Ticket

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
+
+});
+// Route Tickets
+Route::group(['prefix' => 'tickets','as' => 'tickets.','middleware'=>'auth:sanctum'], function(){
+    Route::get('index/{id?}', [TicketController::class, 'index'])->name('index');
+    Route::post('store', [TicketController::class, 'store'])->name('store');
+    Route::put('edit/{id}', [TicketController::class, 'edit'])->name('edit');
+    Route::delete('delete/{id}', [TicketController::class, 'delete'])->name('delete');
 });
