2989af9921cf1e4e9768d4915332bfa60f16f488
Arman <alibabooo1384@gmail.com>
2024-07-08 10:58:48 -0700

    complete the crud of tickets tabele

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function index()
+    {
+        $ticket = Ticket::orderBy('id','desc')->first();
+        return response()->json($ticket);
+    }
+
+
+    public function store(Request $request)
+    {
+        $ticket = ticket::create($request->toArray());
+        return response()->json($ticket);
+    }
+
+
+    public function update(Request $request, string $id)
+    {
+        $ticket = ticket::where('id' , $id)->update($request->toArray());
+        return response()->json($ticket);
+    }
+
+
+    public function destroy(string $id)
+    {
+        $ticket = ticket::where('id' , $id)->delete();
+        return response()->json($ticket);
+    }
 }
