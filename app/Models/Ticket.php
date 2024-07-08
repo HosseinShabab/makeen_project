d0b6b5a6fd56a173de6e331d9fe55b86d60552d5
Arman <alibabooo1384@gmail.com>
2024-07-08 10:40:57 -0700

    complete the model of tickets tabele

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "discription",
+        "time",
+        "response",
+        "user_id"
+
+    ];
 }
