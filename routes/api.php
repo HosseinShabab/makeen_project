ec3773cff08df03571130d7e0d59dddc52431750
nima <andarjaninima@gmail.com>
2024-07-08 20:54:18 +0330

    write user route in api file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//users route
+Route::prefix('users/')->as('users.')->group(function () {
+    Route::get('index', [UserController::class, 'index'])->name('index');
+    Route::post('create', [UserController::class, 'create'])->name('create');
+    Route::put('edit', [UserController::class, 'edit'])->name('edit');
+    Route::delete('delete', [UserController::class, 'delete'])->name('delete');
 });
