a217f5d7db06a0666597be151a669dcf4f0dea20
Arman <alibabooo1384@gmail.com>
2024-07-08 11:54:58 -0700

    Write the crud of message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function index()
+    {
+        $message = Message::orderBy('id','desc')->first();
+        return response()->json($message);
+    }
+
+
+    public function store(Request $request)
+    {
+        $message=  Message::create( $request->toArray());
+        return response()->json(['message'=>$message]);
+    }
+
+
+
+    public function update(Request $request, string $id)
+    {
+        $message = Message::where('id' , $id)->update($request->toArray());
+        return response()->json($message);
+    }
+
+
+    public function destroy(string $id)
+    {
+      $message = Message::where('id' , $id)->delete();
+      return response()->json($message);
+    }
+}
 }
