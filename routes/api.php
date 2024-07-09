51bc2f40c24409bb4ac285809dc99649060233af
hossein <shabab1831@gmail.com>
2024-07-09 22:16:15 +0330

    generating loans route group

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'loans', 'as' => 'loans.'], function () {
+
+    Route::get('index/{id?}', [LoanController::class, 'index'])->name('index');
+    Route::put('edit/{id}', [LoanController::class, 'update'])->name('edit');
+    Route::post('create', [LoanController::class, 'store'])->name('store');
+    Route::delete('delete/{id}', [LoanController::class, 'destroy'])->name('destroy');
+
 });
