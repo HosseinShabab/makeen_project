783df1caa1a5bd3bc6079fa9ddc058aefab9c54b
hossein <shabab1831@gmail.com>
2024-07-26 13:50:32 +0330

    generatin installments migration

---
+++ database/migrations/2024_07_26_100701_create_installments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('installments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('count');
+            $table->unsignedBigInteger('price');
+            $table->date("due_date");
+            $table->enum('status',['paid','unpaid','error'])->default("upaid");
+            $table->enum("admin_accept",["accepted",'faid'])->nullable();
+            $table->text("admin_description")->nullable();
+            $table->unsignedBigInteger('paid_price')->nullable();
+            $table->text("user_description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('installments');
+    }
+};
