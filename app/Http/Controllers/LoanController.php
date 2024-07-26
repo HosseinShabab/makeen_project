916ef63414ffccd8b9370335d12d0cd55645120e
hossein <shabab1831@gmail.com>
2024-07-26 13:13:39 +0330

    fixing function in controller loan

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -24,12 +24,17 @@ class LoanController extends Controller
         $loan_guarantor = DB::table('laon_guarantor')
             ->where("loan_id", $request->loan_id)->where('guarantor_id', $request->guarantor_id)
             ->update(["guarantor_accept" => $request->guarantor_accept]);
+
         $guarnators_accept = DB::table('loan_guarantor')
             ->select('guarantor_accept')->where('loan_id', $request->loan_id)->get();
+
         $temp = "accepted";
+
         foreach ($guarnators_accept as $guarantor_accept) {
+
             if ($guarantor_accept == "pending")
                 $temp = "pending";
+
             if ($guarantor_accept == "faild") {
                 $temp = "faild";
                 break;
@@ -46,10 +51,12 @@ class LoanController extends Controller
     public function showAdmin(Request $request)
     {
         if ($request->count == "checked") {
-            $loans = Loan::where('admin_accept', "!=", null)->where('type', $request->type)
+
+            $loans = Loan::where('admin_accept', "!=", "pending")->where('type', $request->type)
                 ->paginate($request->paginate)->get();
         } else {
-            $loans = Loan::where('admin_accept', null)->where('type', $request->type)
+
+            $loans = Loan::where('admin_accept', "pending")->where('type', $request->type)
                 ->paginate($request->paginate)->get();
         }
         return response()->json($loans);
@@ -57,10 +64,9 @@ class LoanController extends Controller
 
     public function show(Request $request)
     {
-        if ($request->accept_status == "pending") {
-            $loans = Loan::where('accept_status', null)->paginate($request->paginate)->get();
-        }
-        $loans = Loan::where('accept_status', $request->accept_status)->paginate($request->paginate)->get();
+
+        $loans = Loan::where('admin_accept', $request->admin_accept)->paginate($request->paginate)->get();
+
         return response()->json($loans);
     }
 
@@ -69,12 +75,16 @@ class LoanController extends Controller
         $loan = Loan::find($request->loan_id);
         $loan->admin_accept = $request->admin_accept;
         $loan->admin_description = $request->admin_description;
+
         if ($request->admin_accept == "accepted") {
+
             $temp = $request->installment_count;
+
             for ($i = 1; $i <= $temp; $i++) {
                 //create installment ;
             }
         }
+        return response()->json($loan);
     }
 
     public function store(Request $request)
@@ -82,16 +92,16 @@ class LoanController extends Controller
 
         $loan = new Loan();
         $loan->create([
-            "loan_number" => $request->loan_number,
+            "loan_number" => ($request->user()->loans()->count()) + 1,
             "price" => $request->price,
             "user_description" => $request->description,
             "type" => $request->type,
-            "guarantors_accept" => "pending",
             "user_id" => $request->user_id,
         ]);
 
         $guarantors_id = $request->guarantros_id;
         foreach ($guarantors_id as $guarantor_id) {
+
             DB::table("loan_guarantor")->insert(["loan_id" => $loan->id, "guarantor_id" => $guarantor_id]);
             //yek massage sakhte beshe baraye on user :
         }
