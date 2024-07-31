e7ea60b584c2c0be85c84e7ff24233688b181b69
nima <andarjaninima@gmail.com>
2024-07-31 14:44:18 +0330

    completed update profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,18 +71,32 @@ class AuthController extends Controller
         return ['message' => 'successfully logged out have fun'];
     }
 
-    public function updateprofile(UpdateProfileRequest $request, $id)
+    public function updateprofile(UpdateProfileRequest $request)
     {
-        $updateprofile = User::where('id', $id)->update([
-            "name" => $request->name,
-            "last_name" => $request->last_name,
-            "home_number" => $request->home_number,
-            "emergency_number" => $request->emergency_number,
-            "address" => $request->address,
-            "sheba_number" => $request->sheba_number,
-            "card_number" => $request->card_nuumber,
-        ]);
-        return response()->json($updateprofile);
+        $updateprofile = User::find($request->id);
+
+        $updateprofile->first_name = $request->first_name;
+        $updateprofile->last_name = $request->last_name;
+        $updateprofile->home_number = $request->home_number;
+        $updateprofile->emergency_number = $request->emergency_number;
+        $updateprofile->address = $request->address;
+        $updateprofile->sheba_number = $request->sheba_number;
+        $updateprofile->card_number = $request->card_nuumber;
+
+            // $user['first_name'] = $user['first_name'] ?? null;
+            // $user['last_name'] = $user['last_name'] ?? null;
+            // $user['emergency_number'] = $user['emergency_number'] ?? null;
+            // $user['home_number'] = $user['home_number'] ?? null;
+            // $user['card_number'] = $user['card_number'] ?? null;
+            // $user['sheba_number'] = $user['sheba_number'] ?? null;
+            // $user['address'] = $user['address'] ?? null;
+            //     User::create($updateprofile);
+
+        if ($request->filled('name', 'last_name', 'home_number', 'emergency_number',
+         'address', 'sheba_number', 'card_number')){
+         }
+         $updateprofile->save();
+            return response()->json($updateprofile);
     }
 
 
@@ -91,9 +105,8 @@ class AuthController extends Controller
         if (Auth()->check()) {
             return response()->json(auth()->user());
         } else {
-            return response()->json(null,status:401);
+            return response()->json(null, status: 401);
         }
     }
-
 }
-
+    
