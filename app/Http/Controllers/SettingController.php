1336c81fcc1978dad3206bbf3b28d4cefbc51b09
nima <andarjaninima@gmail.com>
2024-08-05 00:51:39 +0330

    completed setting controller

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SettingStoreRequest;
+use App\Http\Requests\SettingUpdateRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 
@@ -14,22 +15,27 @@ class SettingController extends Controller
         return response()->json($setting);
     }
 
-    public function update(){
-
+    public function update(SettingUpdateRequest $request){
+        $setting = Setting::find($request->setting_id);
+        $setting->guarantors_count = $request->input('guarantors_count', $setting->guarantors_count);
+        $setting->loans_count = $request->input('loans_count', $setting->loans_count);
+        $setting->fund_name = $request->input('fund_name', $setting->fund_name);
+        $setting->phone_number = $request->input('phone_number', $setting->phone_number);
+        $setting->card_number = $request->input('card_number', $setting->card_number);
+        $setting->description = $request->input('description', $setting->description);
+        $setting->subscription = $request->input('subscription', $setting->subscription);
+        $setting->save();
+        return response()->json($setting);
     }
 
     public function addmedia(Request $request)
     {
-        $type = $request->type;
         $typable_id = $request->typable_id;
         if ($request->user()->hasRole("admin")) {
-            $type = "admin";
             $typable_id = $request->user()->id;
         }
-        if ($type == 'setting') {
             $setting = Setting::find($typable_id);
-        }
-        $setting = $setting->addMediaFromRequest('media')->toMediaCollection("$request->collection", 'local');
+        $setting = $setting->addMediaFromRequest('media')->toMediaCollection("logo", 'local');
         return response()->json($setting);
     }
 
@@ -39,7 +45,7 @@ class SettingController extends Controller
         return response()->json($setting);
     }
 
-    public function index(Request $request){
+    public function index(){
         $setting = Setting::get();
         return response()->json($setting);
     }
