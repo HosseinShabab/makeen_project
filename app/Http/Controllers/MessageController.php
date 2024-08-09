016352a62eb1bfd4babbd623d2b03a37e4373346
Arman <alibabooo1384@gmail.com>
2024-08-09 13:58:11 -0700

    add newfunction in message controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,12 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\MessageRequest;
 use App\Models\Message;
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 
 class MessageController extends Controller
 {
 
+    private function isTicket($type, $user_id)
+    {
+        $user = User::find($user_id);
+        if (!$user)
+            return false;
+        $ticket = Ticket::where([['user_id', $user_id], ['type', $type]])->first();
+        if (!$ticket) {
+            $ticket = Ticket::create([
+                'type' => $type,
+                'name' => $user->first_name . ' ' . $user->last_name,
+                'response_status' => "responded",
+                'user_id' => $user_id,
+            ]);
+        }
+          return $ticket->id;
+    }
 
-
+    private function pendTicket($ticket_id, $response_status)
+    {
+        $ticket = Ticket::find($ticket_id);
+        $ticket->response_status = $response_status;
+        $ticket->save();
+    }
 }
