26bfa074ba47ffae947eebe1c5aab1f724d33b66
Arman <alibabooo1384@gmail.com>
2024-08-09 13:20:04 -0700

    delete all function

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -10,39 +10,6 @@ use Illuminate\Http\Request;
 class MessageController extends Controller
 {
 
-    public function store(MessageRequest $request)
-    {
-        $user = $request->user();
-        if ($user->hasRole('user')) {
-            $message =  Message::create($request->toArray());
-            return response()->json($message);
-        }
-    }
 
-
-
-    public function index()
-    {
-        $message = Message::get();
-        return response()->json($message);
-    }
-
-
-
-    public function mymessage($id = null)
-    {
-        if ($id) {
-            $message = Message::where('user_id', $id)->orderBy('id', 'desc')->get();
-        } else {
-            $message = Message::where('user_id', auth()->id())->orderBy('id', 'desc')->paginate(10);
-        }
-        return response()->json($message);
-    }
-
-public function unreadmessage()
-{
-    $unreadmessage = Message::where('user_id', auth()->id())->where('status', 'unread')->count();
-    return response()->json($unreadmessage);
-}
 
 }
