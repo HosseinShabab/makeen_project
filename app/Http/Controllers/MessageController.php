895b68144535402950375758ce8864fb337ef3d5
Arman <alibabooo1384@gmail.com>
2024-08-09 14:39:28 -0700

    add function in message controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -29,10 +29,52 @@ class MessageController extends Controller
           return $ticket->id;
     }
 
+
+
+
     private function pendTicket($ticket_id, $response_status)
     {
         $ticket = Ticket::find($ticket_id);
         $ticket->response_status = $response_status;
         $ticket->save();
+    }
+
+
+
+    public function store(MessageRequest $request)
+    {
+        $user = $request->user();
+        $isTicket = $this->isTicket('unsystematic', $user->id);
+        if (!$isTicket)
+            return response()->json(["error" => 'user not valid']);
+        $message = Message::create([
+            'description' => $request->description,
+            'ticket_id' => $isTicket,
+            "status" => "read",
+            'priority' => $request->priority,
+        ]);
+        $this->pendTicket($isTicket, 'pending');
+        return response()->json($message);
+    }
+
+
+
+    
+    public function storeAdmin(MessageRequest $request)
+    {
+        $user_id = $request->user_id;
+        $type = $request->type;
+        $isTicket = $this->isTicket($type, $user_id);
+        if (!$isTicket)
+            return response()->json(['error' => 'user not  valid ']);
+        $message = Message::create([
+            "title" => $request->title,
+            "description" => $request->description,
+            'ticket_id' => $isTicket,
+            'status' => "unread",
+        ]);
+        $this->pendTicket($isTicket, 'responded');
+        return response()->json($message);
+
     }
 }
