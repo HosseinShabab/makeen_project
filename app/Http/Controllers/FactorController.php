84ea2a2f307c9b05eb127fa45eb15ae6dae4a844
hossein <shabab1831@gmail.com>
2024-08-11 09:26:08 +0330

    updating factor controller

--- app/Http/Controllers/FactorController.php
+++ app/Http/Controllers/FactorController.php
@@ -11,7 +11,7 @@ use PDO;
 class FactorController extends Controller
 {
     public function factorCnt(){
-        $factors = Factor::where('acceptd_status',null)->count();
+        $factors = Factor::where('accept_status',null)->count();
         return response()->json($factors);
     }
 
